86bbc94f473fe2778b655a897e55de7b6c36d9c5
Jim Wardlaw <jim.wardlaw@gmail.com>
2012-02-09 16:57:19 +0000

    Added new library file to repo

---
+++ application/libraries/goodform.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * GoodForm
+ * ---
+ * Create nice flexible forms in CodeIgniter
+ *
+ * @licence 	MIT Licence
+ * @category	Librarys 
+ * @author		Jim Wardlaw
+ * @link		http://stwt.co/code/goodform
+ * @version 	2.0
+ */ 
+class Goodform {
+}
+/* End of file goodform.php */
+/* Location: ./application/librarys/goodform.php */
