d8493c87433c5d95280476795da96ad52365293f
Jim Wardlaw <jim.wardlaw@gmail.com>
2012-02-14 16:35:40 +0000

    moved templates to one line each

--- application/config/goodform.php
+++ application/config/goodform.php
@@ -4,30 +4,16 @@ $config['templates']['basic'] = array(
 	'default'	=> '<div class="control-group {state}">{label}{input}{text}</div>',
 	'help'		=> '<span class="help-inline">{text}</span>',
 	'input'		=> '{label}{input}{text}',
+	'button'	=> '{input}{text}',
 	'checkbox'	=> '<div class="control-group {state}">{label}<label class="checkbox">{input}{text}</label></div>',
 	'radio'		=> '<div class="control-group {state}">{label}<label class="radio">{input}{text}</label></div>',	
 );
 
 $config['templates']['horizontal'] = array(
-	'default'	=> '<div class="control-group {state}">
-	{label}
-	<div class="controls">
-		{input}
-		{text}
-	</div>
-</div>',
+	'default'	=> '<div class="control-group {state}">{label}<div class="controls">{input}{text}</div></div>',
 	'help'		=> '<span class="help-inline">{text}</span>',
 	'input'		=> '{label}{input}{text}',
-	'checkbox'	=> '<div class="control-group {state}">
-	{label}
-	<div class="controls">
-		<label class="checkbox">{input}{text}</label>
-	</div>
-</div>',
-	'radio'		=> '<div class="control-group {state}">
-	{label}
-	<div class="controls">
-		<label class="radio">{input}{text}</label>
-	</div>
-</div>',
+	'button'	=> '{input}{text}',
+	'checkbox'	=> '<div class="control-group {state}">{label}<div class="controls"><label class="checkbox">{input}{text}</label></div></div>',
+	'radio'		=> '<div class="control-group {state}">{label}<div class="controls"><label class="radio">{input}{text}</label></div></div>',
 );
